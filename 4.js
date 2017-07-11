b7f585a9a4e42210b473ed68e3ef4c4cbbe6983d
tom jennings <tomjennings100@gmail.com>
2017-07-11 14:55:10 +0100

    changing for prabhjot

--- 4.js
+++ 4.js
@@ -1,4 +1,3 @@
-//You may have heard of the Fizzbuzz problem (if not, see below for a description). 
-// I would like you to write the most interesting, messed up FizzBuzz you can - feel free to use all of the dark secrets of Javascript, break all the rules if you like.
+
 
 //FizzBuzz problem: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
